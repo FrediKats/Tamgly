c36b3d041ff3eee3dd07f1602ff548858dd11fce
Fredi Kats <fredikats@outlook.com>
2022-04-03 21:36:03 +0300

    refactor: add implement IEquatable for TamglyWeek, add constant instead of magic numbers

--- Source/Domain/Kysect.Tamgly.Core/ValueObjects/TamglyWeek.cs
+++ Source/Domain/Kysect.Tamgly.Core/ValueObjects/TamglyWeek.cs
@@ -1,10 +1,12 @@
 ﻿namespace Kysect.Tamgly.Core.ValueObjects;
 
-public readonly struct TamglyWeek
+public readonly struct TamglyWeek : IEquatable<TamglyWeek>
 {
+    private const int DayInWeek = 7;
+
     public int WeekNumber { get; }
-    public DateTime Start => TamglyTime.ZeroDay.AddDays(7 * WeekNumber);
-    public DateTime End => TamglyTime.ZeroDay.AddDays(7 * (WeekNumber +1)).AddTicks(-1);
+    public DateTime Start => TamglyTime.ZeroDay.AddDays(DayInWeek * WeekNumber);
+    public DateTime End => TamglyTime.ZeroDay.AddDays(DayInWeek * (WeekNumber +1)).AddTicks(-1);
 
     public TamglyWeek(int weekNumber)
     {
@@ -13,12 +15,27 @@ public readonly struct TamglyWeek
 
     public static TamglyWeek FromDate(DateTime dateTime)
     {
-        var weekCount = TamglyTime.ZeroDay.DaysTo(dateTime) / 7;
+        var weekCount = TamglyTime.ZeroDay.DaysTo(dateTime) / DayInWeek;
         return new TamglyWeek(weekCount);
     }
 
     public bool Contains(DateTime dateTime)
     {
         return Start <= dateTime && dateTime <= End;
+    }
+
+    public bool Equals(TamglyWeek other)
+    {
+        return WeekNumber == other.WeekNumber;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is TamglyWeek other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return WeekNumber;
     }
 }
