1b4be1e2ee9c907dd98f15c7731d0268b62297a5
Fredi Kats <fredikats@outlook.com>
2022-05-07 20:59:37 +0300

    feat: add command for creating WI

---
+++ Source/Presentation/Kysect.Tamgly.ConsoleClient/ProofOfConcept.cs
@@ -0,0 +1,22 @@
+﻿using Kysect.Tamgly.ConsoleClient.Commands;
+using Spectre.Console.Cli;
+
+namespace Kysect.Tamgly.ConsoleClient;
+
+public class ProofOfConcept
+{
+    public void Execute()
+    {
+        var app = new CommandApp();
+        app.Configure(config =>
+        {
+            config.AddCommand<CreateWorkItemCommand>("create-wi");
+        });
+
+        while (true)
+        {
+            string? readLine = Console.ReadLine() ?? throw new ArgumentException();
+            app.Run(readLine.Split());
+        }
+    }
+}
